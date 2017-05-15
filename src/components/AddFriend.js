547bd506bdc15cb6f2e4ea0ac74173cab48823c2
andrasat <andra.satria1@gmail.com>
2017-05-15 11:16:23 +0700

    added addfriendbutton, working on add friend input

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {  } from 'react-native'
-import { Container, Content, Header, Left, Body, Right, Input, Form, Icon, Text } from 'native-base'
+import { ToastAndroid } from 'react-native'
+import { Container, Content, Header, Left, Body, Right, Input, Item, Icon, Text, Button } from 'native-base'
 
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
@@ -9,15 +9,43 @@ import {} from '../actions'
 
 class UserList extends React.Component {
 
+  state = {
+    friendID: ''
+  }
+
+  handleFriendIDInput = text => {
+    this.setState({
+      friendID: text
+    })
+  }
+
   render() {
-    const {} = this.props
+    const { _setModalAddFriendVisible } = this.props
+    const { friendID } = this.state
     return (
       <Container>
         <Header>
-
+          <Left>
+            <Button light transparent iconLeft
+              onPress={()=> _setModalUserListVisible(false)}>
+              <Icon name="arrow-back" color="#FFF"/>
+              <Text>Back</Text>
+            </Button>
+          </Left>
         </Header>
         <Content>
-
+          <Item inlineLabel>
+            <Label>ID</Label>
+            <Input
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={friendID}
+              onChange={(e) => this.handleFriendIDInput(e.nativeEvent.text)}
+              />
+            <Button bordered>
+              <Icon name="ion-plus" color="#292988"/>
+            </Button>
+          </Item>
         </Content>
       </Container>
     )
