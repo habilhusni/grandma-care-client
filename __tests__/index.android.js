2193c35ade6269ceb41d3b2f7545239d66e5e183
habilhusni <you@example.com>
2017-05-16 00:28:36 +0700

    added some props testing

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,6 +1,15 @@
 import 'react-native';
 import React from 'react';
 import client from '../index.android.js';
+import { Provider } from 'react-redux';
+import reducers from '../src/reducers';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+
+import App from '../src/index.js';
+import { StackNavigator } from 'react-navigation';
+
 import AddFriend from '../src/components/AddFriend.js';
 import AddFriendButton from '../src/components/AddFriendButton.js';
 import Login from '../src/components/Login.js';
@@ -12,7 +21,9 @@ import SettingPage from '../src/components/SettingPage.js';
 import SplashScreen from '../src/components/SplashScreen.js';
 import UserList from '../src/components/UserList.js';
 import UserListButton from '../src/components/UserListButton.js';
+import sum from '../math.js';
 
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
@@ -22,7 +33,11 @@ it('renders with snapshot', () => {
 });
 
 it('renders AddFriend snapshot', () => {
-  const compAddFriend = renderer.create(<AddFriend/>);
+  const compAddFriend = renderer.create(
+    <Provider store={store}>
+      <AddFriend/>
+    </Provider>
+  );
   expect(compAddFriend).toMatchSnapshot();
 });
 
@@ -32,7 +47,11 @@ it('renders AddFriendButton snapshot', () => {
 });
 
 it('renders Login snapshot', () => {
-  const compLogin = renderer.create(<Login/>);
+  const compLogin = renderer.create(
+    <Provider store={store}>
+      <Login/>
+    </Provider>
+  );
   expect(compLogin).toMatchSnapshot();
 });
 
@@ -42,36 +61,70 @@ it('renders LogoutButton snapshot', () => {
 });
 
 it('renders Main snapshot', () => {
-  const compMain = renderer.create(<Main/>);
+  const compMain = renderer.create(
+    <Provider store={store}>
+      <App>
+        <Main/>
+      </App>
+    </Provider>
+  );
   expect(compMain).toMatchSnapshot();
 });
 
 it('renders Maps snapshot', () => {
-  const compMaps = renderer.create(<Maps/>);
+  const compMaps = renderer.create(
+    <Provider store={store}>
+      <App>
+        <Maps/>
+      </App>
+    </Provider>
+  );
   expect(compMaps).toMatchSnapshot();
 });
 
 it('renders Register snapshot', () => {
-  const compRegister = renderer.create(<Register/>);
+  const compRegister = renderer.create(
+    <Provider store={store}>
+      <Register/>
+    </Provider>
+  );
   expect(compRegister).toMatchSnapshot();
 });
 
 it('renders SettingPage snapshot', () => {
-  const compSettingPage = renderer.create(<SettingPage/>);
+  const compSettingPage = renderer.create(
+    <Provider store={store}>
+      <SettingPage/>
+    </Provider>
+  );
   expect(compSettingPage).toMatchSnapshot();
 });
 
 it('renders SplashScreen snapshot', () => {
-  const compSplashScreen = renderer.create(<SplashScreen/>);
+  const compSplashScreen = renderer.create(
+    <Provider store={store}>
+      <SplashScreen/>
+    </Provider>
+  );
   expect(compSplashScreen).toMatchSnapshot();
 });
 
 it('renders UserList snapshot', () => {
-  const compUserList = renderer.create(<UserList/>);
+  const compUserList = renderer.create(
+    <Provider store={store}>
+      <App>
+        <UserList/>
+      </App>
+    </Provider>
+  );
   expect(compUserList).toMatchSnapshot();
 });
 
 it('renders UserListButton snapshot', () => {
   const compUserListButton = renderer.create(<UserListButton/>);
   expect(compUserListButton).toMatchSnapshot();
+});
+
+test('Adds two number', () => {
+  expect(sum(2,3)).toBe(5)
 });
