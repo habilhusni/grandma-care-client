7a63e3de6e7cec5be3c00faded9180cb195d60ed
andrasat <andra.satria1@gmail.com>
2017-05-15 16:36:17 +0700

    working on delete friend action

--- src/components/DeleteFriendButton.js
+++ src/components/DeleteFriendButton.js
@@ -1,6 +1,19 @@
 import React from 'react'
-import {  } from 'native-base'
+import { Button, Icon} from 'native-base'
 
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-class DeleteButton
+class DeleteFriendButton extends React.Component {
+
+  render() {
+    return (
+      <Button bordered danger
+        onPress={() => null}>
+        <Icon name="delete" android="md-close" color="#FF3135"/>
+      </Button>
+    )
+  }
+}
+
+export default DeleteFriendButton
