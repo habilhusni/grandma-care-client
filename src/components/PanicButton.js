b5f7b03979891bdea1cca8e62a4f721f91b68cf4
andrasat <andra.satria1@gmail.com>
2017-05-16 10:22:32 +0700

    panic button working as intended

--- src/components/PanicButton.js
+++ src/components/PanicButton.js
@@ -1,27 +1,42 @@
 import React from 'react'
+import { ToastAndroid } from 'react-native'
 import { Fab, Icon } from 'native-base'
 
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
+import { updateSensor } from '../actions'
 
 class PanicButton extends React.Component {
 
   render() {
+    const { updateSensor, token, userID } = this.props
     return (
       <Fab
         active={false}
         style={{backgroundColor:'#FF2821'}}
         position="bottomLeft"
-        onPress={()=> null}>
-        <Icon name="alert" android="md-alert" />
+        onPress={()=> {
+          updateSensor({
+            token, userID,
+            x: 0, y: 0, z:0
+          })
+          ToastAndroid.showWithGravity('You just pressed Panic Button !', ToastAndroid.SHORT, ToastAndroid.CENTER)
+        }}>
+        <Icon name="alert" android="md-alert" style={{fontSize:55}} />
       </Fab>
     )
   }
 }
 
-// const mapDispatchToProps = dispatch => ({
-//
-// })
+PanicButton.propTypes = {
+  updateSensor: PropTypes.func.isRequired,
+  token: PropTypes.string.isRequired,
+  userID: PropTypes.string.isRequired
+}
 
-export default PanicButton
+const mapDispatchToProps = dispatch => ({
+  updateSensor: (obj) => dispatch(updateSensor(obj))
+})
+
+export default connect(null, mapDispatchToProps)(PanicButton)
